30932e6a3cc4ef37e14b9f11d7ca14bd7c915004
Tom Horton <thomasphorton@gmail.com>
2018-06-05 01:28:49 -0700

    Changes indices in Dynamo tables.

--- app/app.js
+++ app/app.js
@@ -19,104 +19,190 @@ app.use(function(req, res, next) {
   next()
 });
 
-app.get('/time', function(req, res) {
-    res.status(200).json({
-        "time": moment().tz("CST").format("dddd, MMMM Do YYYY, hh:mm:ss zz")
-    });
-})
+app.get('/trips', function(req, res) {
+  const docClient = new AWS.DynamoDB.DocumentClient();
 
-app.get('/time/:tz', function(req, res) {
-    var timezone = req.params.tz
-    var time = moment().format()
-    try {
-        time = moment().tz(timezone).format("dddd, MMMM Do YYYY, hh:mm:ss z")
-    } catch (ex) {
-        console.log(ex)
+  var params = {
+    TableName: 'mariah-trips-2',
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': '1'
     }
-    res.status(200).json({
-        "time": time
-    });
+  };
+
+  docClient.query(params, (err, data) => {
+    if (err) {
+      console.log("Unable to query. Error: ", JSON.stringify(err, null, 2));
+      res.status(500).json({
+        "err": err
+      })
+    } else {
+      console.log("Query succeeded.");
+      console.log(data);
+
+      let trips = data.Items;
+
+      trips.sort((a, b) => {
+        return b.ts - a.ts;
+      });
+
+      res.status(200).json(trips);
+    }
+  })
+
 })
 
 app.get('/trip/:tripId', (req, res) => {
 
-  var tripId = req.params.tripId;
+  var state = {
+    tripId: req.params.tripId
+  };
 
-  const s3 = new AWS.S3();
-  const docClient = new AWS.DynamoDB.DocumentClient();
+  // get trip data
+  getTripData(state)
+  // if trip hasn't been processed
 
-  let geojson = {
-    'id': 'route',
-    'type': 'line',
-    'source': {
-      'type': 'geojson',
-      'data': {
-        'type': 'Feature',
-        'properties': {},
-        'geometry': {
-          'type': 'LineString',
-          'coordinates': []
-        }
-      }
-    },
-    "layout": {
-        "line-join": "round",
-        "line-cap": "round"
-    },
-    "paint": {
-        "line-color": "#888",
-        "line-width": 8
-    }
-  }
+    // create geojson file
+    .then(state => createTripGeoJson(state))
+  //
+  //   // upload file to s3
+    .then(state => uploadGeoJson(state))
+  //   // update trip table with pointer
+    .then(state => updateTripData(state))
+  // // return trip data
+  .then(state => res.status(200).json(state))
 
-  var params = {
-    TableName:  'mariah-trip-data',
-    KeyConditionExpression: 'tripId = :trip_id',
-    ExpressionAttributeValues: {
-      ':trip_id': tripId
-    }
-  }
-
-  docClient.query(params, (err, data) => {
-    if (err) {
-      console.error(JSON.stringify(err, null, 2));
-    } else {
-
-      console.log(data);
-
-      console.log('success');
-      let coords = [];
-      data.Items.forEach(item => {
-        if (item.lon !== undefined && item.lat !== undefined) {
-          coords.push([item.lon, item.lat]);
-        }
-      })
-
-      geojson.source.data.geometry.coordinates = coords;
-
-      var s3params = {
-        Body: JSON.stringify(geojson),
-        Bucket: 'mariah-us-west-2',
-        Key: `${tripId}.json`
-      }
-
-      s3.putObject(s3params, (err, data) => {
-        if (err) console.log(err, err.stack)
-        else console.log(data);
-
-        let dataUrl = `https://s3-us-west-2.amazonaws.com/mariah-us-west-2/${tripId}.json`;
-
-        res.status(200).json({
-          "tripId": tripId,
-          "data": dataUrl
-        })
-      })
-    }
-  })
 })
 
-app.listen(3000, function() {
-    console.log("App started")
+function getTripData(state) {
+  return new Promise((resolve, reject) => {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    var params = {
+      TableName: 'mariah-trips-2',
+      Key: {
+        userId: '1',
+        tripId: state.tripId
+      }
+    };
+
+    console.log('trip params: ', params);
+
+    docClient.get(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        reject(err);;
+      } else {
+        console.log('getTripData:', data);
+        resolve(state);
+      }
+    })
+
+  })
+}
+
+function createTripGeoJson(state) {
+  return new Promise((resolve, reject) => {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    let geojson = {
+      'type': 'LineString',
+      'coordinates': []
+    };
+
+    var params = {
+      TableName:  'mariah-trip-data-3',
+      KeyConditionExpression: 'tripId = :trip_id',
+      ExpressionAttributeValues: {
+        ':trip_id': state.tripId
+      }
+    }
+
+    docClient.query(params, (err, data) => {
+      if (err) {
+        console.error(JSON.stringify(err, null, 2));
+        reject(err);
+      } else {
+        console.log(data);
+        let coords = [];
+        data.Items.forEach(item => {
+          if (item.lon !== undefined && item.lat !== undefined) {
+            coords.push([parseFloat(item.lon), parseFloat(item.lat)]);
+          }
+        });
+
+        geojson.coordinates = coords;
+        state.ts = data.ts;
+        state.geojson = geojson;
+        resolve(state);
+      }
+    });
+  });
+}
+
+function uploadGeoJson(state) {
+  return new Promise((resolve, reject) => {
+    const s3 = new AWS.S3();
+
+    let s3params = {
+      Body: JSON.stringify(state.geojson),
+      Bucket: 'mariah-us-west-2',
+      Key: `${state.tripId}.json`,
+      ACL: 'public-read'
+    };
+
+    s3.putObject(s3params, (err, data) => {
+      if (err) {
+        console.log(err, err.stack);
+        reject(err);
+      } else {
+        let dataUrl = `https://s3-us-west-2.amazonaws.com/mariah-us-west-2/${state.tripId}.json`;
+        state.dataUrl = dataUrl;
+        delete state.geojson;
+        resolve(state);
+      }
+    });
+  });
+}
+
+function updateTripData(state) {
+  return new Promise((resolve, reject) => {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    var params = {
+      TableName: 'mariah-trips-2',
+      Key: {
+        userId: '1',
+        tripId: state.tripId
+      },
+      AttributeUpdates: {
+        'geoJsonUrl': {
+          Action: 'PUT',
+          Value: state.dataUrl
+        }
+      }
+    };
+
+    console.log(params);
+
+    docClient.update(params, (err, data) => {
+      if (err) {
+        console.log("Unable to query. Error: ", JSON.stringify(err, null, 2));
+        reject(err);
+      } else {
+        console.log("Update succeeded.");
+        console.log(data);
+
+        resolve(state);
+      }
+    })
+  })
+}
+
+var port = 3001;
+
+app.listen(port, function() {
+    console.log(`Application listening on http://localhost:${port}`)
 })
 
 // Export the app object. When executing the application local this does nothing. However,
